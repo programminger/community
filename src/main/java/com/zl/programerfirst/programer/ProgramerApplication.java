19b9b4edc822b59ca716a0700bdee56bc82ac0ae
zlcoder <599660411@qq.com>
2019-07-24 19:36:24 +0800

    init repo

---
+++ src/main/java/com/zl/programerfirst/programer/ProgramerApplication.java
@@ -0,0 +1,13 @@
+package com.zl.programerfirst.programer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramerApplication.class, args);
+    }
+
+}
